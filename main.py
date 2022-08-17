779278e9d7dca1d82a267fe9a2c04b8e4fa9fb9a
truezed <metruezed@gmail.com>
2022-08-17 22:40:21 +0300

    fixed plain token on getenv

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 import re
 
 import telebot
 
-bot = telebot.TeleBot("5585456773:AAHTqq_grxE9hJF7nKX3FU5RoxJuoFPUpyQ")
+bot = telebot.TeleBot(os.getenv("token"))
 
 ban_words = ["крым", "крим", "днр", "лнр", "тайвань", "китай"]
 
@@ -11,7 +12,6 @@ ban_words = ["крым", "крим", "днр", "лнр", "тайвань", "ки
 def echo_all(message):
     print(message)
     text = re.sub('[^A-Za-z0-9а-яА-я]+', '', message.text.lower())
-    print(text)
     if any(word in text for word in ban_words):
         bot.delete_message(message_id=message.message_id, chat_id=message.chat.id)
         bot.send_message(chat_id=message.chat.id, text=f"{message.from_user.first_name}, вы пишете гадости  .ю!")
